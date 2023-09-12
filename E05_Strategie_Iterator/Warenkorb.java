2c1671077250e5553eb2fe7e80efd4ed227415a6
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-09-12 12:09:02 +0200

    solved 5.3

--- E05_Strategie_Iterator/Warenkorb.java
+++ E05_Strategie_Iterator/Warenkorb.java
@@ -1,4 +1,22 @@
 package E05_Strategie_Iterator;
 
 public class Warenkorb {
+    // Fields
+    private Zahlungsstrategie zahlungsstrategie;
+
+    // Constructor
+    public Warenkorb(){
+
+    }
+
+    // Methods
+    // Getters
+    public Zahlungsstrategie getZahlungsstrategie() {
+        return this.zahlungsstrategie;
+    }
+
+    // Setters
+    public void setZahlungsstrategie(Zahlungsstrategie zahlungsstrategie) {
+        this.zahlungsstrategie = zahlungsstrategie;
+    }
 }
