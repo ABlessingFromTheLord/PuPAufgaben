762d33ea750901cc9908f0e4137be7e58f1d62c2
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-09-14 08:34:18 +0200

    added class for 7.1

---
+++ E07_Command_Decorator/Decorator/Testklasse.java
@@ -0,0 +1,11 @@
+package E07_Command_Decorator.Decorator;
+
+public class Testklasse {
+    public static void main(String[] args) {
+        // QNS:
+        // the interface is not completely implemented in subclasses
+        // Waeldlaeufer, Magier and not at all in TrankEffectDecorator
+        // just leave them without body?
+
+    }
+}
