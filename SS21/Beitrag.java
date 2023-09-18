ed4b68d9c2c44602fa14560b5187960d1deab362
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-09-18 22:26:57 +0200

    added classes for WS2122 1

---
+++ SS21/Beitrag.java
@@ -0,0 +1,25 @@
+package SS21;
+
+public class Beitrag implements IBeitrag{
+    // Fields
+    private String name;
+    public String inhalt;
+
+    // Constructor
+    public Beitrag(String name, String inhalt) {
+        this.name = name;
+        this.inhalt = inhalt;
+    }
+
+    // Methods
+    @Override
+    public String getNutzname() {
+        return this.name;
+    }
+
+    @Override
+    public String getInhalt() {
+        return this.inhalt;
+    }
+
+}
---
+++ SS21/IBeitrag.java
@@ -0,0 +1,6 @@
+package SS21;
+
+public interface IBeitrag {
+    public String getNutzname();
+    public String getInhalt();
+}
