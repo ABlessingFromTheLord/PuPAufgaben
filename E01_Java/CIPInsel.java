064aa8fd6bbed5a837ef5e253efc724b9e29ce5f
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-12-05 10:17:39 +0100

    miscellaneous

--- E01_Java/CIPInsel.java
+++ E01_Java/CIPInsel.java
@@ -1,7 +1,11 @@
 package E01_Java;
 
 public class CIPInsel {
+
+    // &begin[This]
     public String name;
+    // &end[This]
+
     public Computer[] computer = new Computer[10];
 
     public CIPInsel(String n){
